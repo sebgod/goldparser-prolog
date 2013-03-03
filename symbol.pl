7a022a703b42a9b0c0e7f5f3344477c49f240d58
sebgod <sebastian.godelet+githut@gmail.com>
2013-03-03 07:29:08 +0100

    Update symbol.pl
    tabwidth

--- symbol.pl
+++ symbol.pl
@@ -5,24 +5,24 @@
 
 :- use_module(table, []).
 
-%%  Normal Nonterminal
+%% Normal Nonterminal
 type(0, nonterminal).
-%%	Normal Terminal
+%% Normal Terminal
 type(1, terminal).
-%%	Noise terminal. These are ignored by the parser.
+%% Noise terminal. These are ignored by the parser.
 % Comments and whitespace are considered 'noise'.
 type(2, noise).
-%%	End Character - End of File.
+%% End Character - End of File.
 % This symbol is used to represent the end of the file
 % or the end of the source input.
 type(3, eof).
-%%	 Lexical group start.
+%% Lexical group start.
 type(4, group_start).
-%%	Lexical group end. Groups can end with normal terminals as well.
+%% Lexical group end. Groups can end with normal terminals as well.
 type(5, ground_end).
-%%	This was used in the CGT file format. It is not used in EGT.
+%% This was used in the CGT file format. It is not used in EGT.
 type(6, decremented).
-%%	Error Terminal. If the parser encounters an error reading a token,
+%% Error Terminal. If the parser encounters an error reading a token,
 % this kind of symbol can used to differentiate it from other terminal types
 type(7, error).
 
