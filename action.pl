03284e93b4386876230cf06091d5b70cf8e75a69
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-03-03 08:32:14 +0100

    remove pldoc warning
    - wrote the first structured comment (this should be really in the
    language ...)

--- action.pl
+++ action.pl
@@ -2,27 +2,33 @@
 
 :- use_module(entries, []).
 
-%% This action indicates the symbol is to be shifted.
+%%	type(+Number:int, +Name:atom) is det.
+%%	type(?Number:int, ?Name:atom) is nondet.
+%
+% shift: This action indicates the symbol is to be shifted.
 % The Target field will contain the index of the state
 % in the LALR State table that the parsing engine will advance to.
-type(1, shift).
-
-%% This action denotes that the parser can reduce a rule.
-% The Target field will contain the index % of the rule
-% in the Rule Table.
-type(2, reduce).
-
-%% This action is used when a rule is reduced
+% The Target field will contain the index
+% of the rule in the Rule Table.
+%
+% reduce: This action denotes that the parser can reduce a rule.
+%
+% goto: This action is used when a rule is reduced
 % and the parser jumps to the state that represents
-% the shifted nonterminal.% The Target field will contain the
+% the shifted nonterminal.
+% The Target field will contain the
 % index of the state in the LALR State table that the parsing engine will
 % jump to after a reduction if completed.
-type(3, goto).
+%
+% accept: When the parser encounters the Accept action for a given
+% symbol, the source text is accepted as correct and complete. In this
+% case, the Target field is not needed and should be ignored.
 
-%% When the parser encounters the Accept action for a given symbol,
-% the source text is accepted as correct and complete.
-% In this case, the Target field is not needed and should be ignored.
+type(1, shift).
+type(2, reduce).
+type(3, goto).
 type(4, accept).
+
 
 find(Actions, SymbolIndex, Action) :-
 	once(
--- symbol.pl
+++ symbol.pl
@@ -5,26 +5,43 @@
 
 :- use_module(table, []).
 
-%% Normal Nonterminal
-type(0, nonterminal).
-%% Normal Terminal
-type(1, terminal).
-%% Noise terminal. These are ignored by the parser.
+%%	type(+Number:int, +Name:atom) is det.
+%%	type(?Number:int, ?Name:atom) is nondet.
+%
+% Represents the symbol types used in the gold parser version 5.0
+%
+% nonterminal: Normal Nonterminal
+%
+% terminal:    Normal Terminal
+%
+% noise:       Noise terminal. These are ignored by the parser.
 % Comments and whitespace are considered 'noise'.
-type(2, noise).
-%% End Character - End of File.
+%
+% eof:         End Character - End of File.
 % This symbol is used to represent the end of the file
 % or the end of the source input.
+%
+% group_start: Lexical group start.
+%
+% group_end:   Lexical group end. Groups can end with normal terminals
+% as well.
+%
+% decremented: This was used in the CGT file format. It is not used in
+% EGT.
+%
+% error: Error Terminal. If the parser encounters an error reading a
+%  token, this kind of symbol can used to differentiate it from other
+%  terminal types
+
+type(0, nonterminal).
+type(1, terminal).
+type(2, noise).
 type(3, eof).
-%% Lexical group start.
 type(4, group_start).
-%% Lexical group end. Groups can end with normal terminals as well.
 type(5, ground_end).
-%% This was used in the CGT file format. It is not used in EGT.
 type(6, decremented).
-%% Error Terminal. If the parser encounters an error reading a token,
-% this kind of symbol can used to differentiate it from other terminal types
 type(7, error).
+
 
 by_type(Tables, Kind, SymbolIndex, Symbol) :-
 	table:items(symbol_table, Tables, SymbolIndex, Symbol),
