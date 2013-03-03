538a775001fa228e84d9371881269d11342bfe80
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-03-03 17:45:27 +0100

    enhanced AST module
    added: peek/2,pop/3

--- ast.pl
+++ ast.pl
@@ -1,5 +1,9 @@
-:- module(ast, [empty/1 ,push/3]).
+:- module(ast, [empty/1 ,push/3, peek/2, pop/3]).
 
 empty(ast([], [])).
 
 push(ast(Stack, Tree), Terminal, ast([Terminal | Stack], Tree)).
+
+peek(ast([Top | _], _Tree), Top).
+
+pop(ast([Top | StackR], Tree), Top, ast(StackR, Tree)).
