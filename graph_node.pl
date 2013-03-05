0282b0dcd712eb423f8cb84587b19f77573f13ac
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-03-05 08:07:15 +0100

    moved classes to their own files
    using prolog terms as arguments  for graph_viewer

---
+++ graph_node.pl
@@ -0,0 +1,39 @@
+:- module(graph_node, []).
+
+:- use_module(library(pce)).
+
+:- pce_begin_class(graph_node(name), device).
+
+handle(w/2, 0, link, link).
+
+initialise(Node, Name:name) :->
+	"Create from name"::
+	send(Node, send_super, initialise),
+	send(Node, display, circle(60), point(-30, -30)),
+	send(Node, display, new(T, text(Name, center))),
+	send(T, center, point(0, 0)),
+	send(Node, send_super, name, Name).
+
+name(Node, Name:name) :->
+	"Change name of a node"::
+	get(Node, member, text, Text),
+	send(Text, string, Name),
+	send(Node, send_super, name, Name).
+
+:- pce_global(@graph_node_recogniser, make_graph_node_recogniser).
+
+make_graph_node_recogniser(R) :-
+	new(R, move_gesture(left)),
+	send(R, condition,
+	     ?(@event?position, distance, point(0,0)) < 30).
+
+
+event(Node, Ev:event) :->
+	"Make it movable"::
+	(   send(@graph_node_recogniser, event, Ev)
+	->  true
+	;   send(Node, send_super, event, Ev)
+	).
+
+:- pce_end_class.
+
--- view_parser.pl
+++ view_parser.pl
@@ -1,7 +1,8 @@
 
 :- module(view_parser,
-	  [ view_parser/1
-	  ]).
+	  [ view_parser/1 ]).
+
+:- pce_autoload(graph_viewer, graph_viewer).
 
 :- use_module(library(pce)).
 :- use_module(table, []).
@@ -14,12 +15,13 @@
 	   , term_to_atom/2
 	   ]).
 
+
 view_parser(Parser) :-
 	Parser = parser(grammar(_Header, _), _, _),
-	new(GV, graph_viewer),
+	new(GV, graph_viewer(Parser, display)),
 	send(GV, open),
 	send(GV, status, full_screen),
-	send(GV, generate, Parser).
+	send(GV, generate).
 
 display(Parser, From, To) :-
 	member(Kind, [display_lalr_states, display_rules]),
@@ -97,121 +99,16 @@ display_symbol_format(nonterminal, '<~w>') :- !.
 display_symbol_format(_, '~w').
 
 
-:- pce_begin_class(graph_viewer, frame).
 
 
-initialise(GV) :->
-	"Create graph-viewer"::
-	send(GV, send_super, initialise, 'GOLD Parser Viewer'),
-	send(GV, append, new(P, picture)),
-	send(new(D, dialog), below, P),
-	fill_dialog(D).
-
-fill_dialog(D) :-
-	new(Frame, D?frame),
-	send(D, append, label(reporter)),
-	send(D, append, button(quit, message(Frame, destroy))),
-	%send(D, append, button(clear, message(Frame, clear))),
-	send(D, append, button(postscript, message(Frame, postscript))),
-	send(D, append, button(layout, message(Frame, layout))).
 
 
-clear(F) :->
-	"Clear the diagram"::
-	get(F, member, picture, P),
-	send(P, clear).
 
 
-layout(F) :->
-	"Run graph layout"::
-	get(F, member, picture, P),
-	(   get(P?graphicals, head, Head)
-	->  send(Head, layout)
-	;   send(F, report, error, 'No graph to layout')
-	).
 
 
-:- pce_autoload(finder, library(find_file)).
-:- pce_global(@finder, new(finder)).
-
-postscript(F) :->
-	"Create PostScript in file"::
-	get(@finder, file, @off, '.eps', FileName),
-	get(F, member, picture, Pict),
-	new(File, file(FileName)),
-	send(File, open, write),
-	send(File, append, Pict?postscript),
-	send(File, close),
-	send(File, done),
-	send(F, report, status, 'Saved PostScript in %s', FileName).
-
-generate(F, Parser:term) :->
-	"Create graph using generator"::
-	send(F, clear),
-	forall(display(Parser, From, To),
-		   send(F, display_arc, From, To
-			   )),
-	send(F, layout).
 
 
-:- pce_global(@graph_link, new(link(link, link, line(0,0,0,0,second)))).
-
-display_arc(F, From:name, To:name) :->
-	"Display arc From -> To"::
-	get(F, node, From, NF),
-	get(F, node, To, TF),
-	send(NF, connect, TF, @graph_link).
 
 
-node(F, Name:name, Node:graph_node) :<-
-	"Get (create) node with specified name"::
-	get(F, member, picture, Picture),
-	(   get(Picture, member, Name, Node)
-	->  true
-	;   get(Picture, visible, area(X, Y, W, H)),
-		MX is X + W,
-		MY is Y + H,
-		random(X, MX, NX),
-		random(Y, MY, NY),
-		send(Picture, display, new(Node, graph_node(Name)), point(NX, NY))
-	).
-
-
-:- pce_end_class.
-
-
-:- pce_begin_class(graph_node(name), device).
-
-handle(w/2, 0, link, link).
-
-initialise(Node, Name:name) :->
-	"Create from name"::
-	send(Node, send_super, initialise),
-	send(Node, display, circle(60), point(-30, -30)),
-	send(Node, display, new(T, text(Name, center))),
-	send(T, center, point(0, 0)),
-	send(Node, send_super, name, Name).
-
-name(Node, Name:name) :->
-	"Change name of a node"::
-	get(Node, member, text, Text),
-	send(Text, string, Name),
-	send(Node, send_super, name, Name).
-
-:- pce_global(@graph_node_recogniser, make_graph_node_recogniser).
-
-make_graph_node_recogniser(R) :-
-	new(R, move_gesture(left)),
-	send(R, condition,
-	     ?(@event?position, distance, point(0,0)) < 30).
-
-
-event(Node, Ev:event) :->
-	"Make it movable"::
-	(   send(@graph_node_recogniser, event, Ev)
-	->  true
-	;   send(Node, send_super, event, Ev)
-	).
-
-:- pce_end_class.
 
