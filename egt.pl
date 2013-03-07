62e888ae22a661cfc36ff200c5ea0fa06df17468
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-03-07 10:31:09 +0100

    added notice for "assoc dcg"
    The read_structure//1 is not a real DCG in the list sense, but it is
    sufficient for assoc manipulation.

--- egt.pl
+++ egt.pl
@@ -16,6 +16,9 @@ read_stream(grammar(Header, Assoc), Stream) :-
     empty_assoc(Empty),
     read_structures(Stream, Empty, Assoc).
 
+%%	read_structures(+Stream:stream)// is det.
+% This is not a dcg but rather a convenient way of passing the
+% changed AVL tress (library(assoc)).
 read_structures(Stream) -->
     read_structure(Stream),
     !,
