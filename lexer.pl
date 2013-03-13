70cac3ad4fcfc7dc6205b6493d8ba536b22d5501
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-03-13 08:11:22 +0100

    trying improved lexer
    removed state assoc

--- lexer.pl
+++ lexer.pl
@@ -18,7 +18,8 @@ scan_stream(Program, Tokens, Stream) :-
     scan_list(Program, Tokens, Input).
 
 scan_list(program(parser(G, T), State, _AST), Tokens, Input) :-
-    state:merge(State, [last_accept-none, chars-''], Initial),
+    state:current(State, dfa-DfaIndex),
+    Initial = lexer(dfa-DfaIndex, last_accept-none, chars-''),
     scan_list_(parser(G, T), Initial, Tokens, Input).
 
 scan_list_(parser(_G, Tables), _State, [Index-''], []) :-
@@ -44,30 +45,25 @@ debug_token_read(Parser, Token) :-
 try_restore_input([], [], []).
 try_restore_input([Skipped | InputR], Skipped, InputR).
 
-eat_token(parser(G, Tables), State, Token) -->
-    {
-     !,
-     dfa:current(Tables, State, DFA),
-     state:current(State, chars-Chars0),
-     state:current(State, last_accept-LastAccept)
-    },
+eat_token(parser(G, Tables),
+          lexer(dfa-DFAIndex, last_accept-LastAccept, chars-Chars0),
+          Token) -->
     (   [Input],
-        { char_and_code(Input, Char, Code),
+        {
+          dfa:current(Tables, DFAIndex, DFA),
+          char_and_code(Input, Char, Code),
           dfa:find_edge(Tables, DFA, Code, TargetIndex)
         }
     ->  { table:item(dfa_table, Tables, TargetIndex, TargetDFA),
           dfa:accept(TargetDFA, Accept),
           atom_concat(Chars0, Char, Chars),
-          state:merge(State,
-                      [chars-Chars,
-                       dfa-TargetIndex,
-                       last_accept-Accept
-                      ], NewState)
+          NewState = lexer(dfa-TargetIndex,
+                           last_accept-Accept,
+                           chars-Chars)
         },
         eat_token(parser(G, Tables), NewState, Token)
     ;   {LastAccept \= none,
-         Token = LastAccept-Chars0,
-         state:merge(State, [last_accept-none, chars-Char], NewState)
+         Token = LastAccept-Chars0
         }
     ).
 
